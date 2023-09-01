c961017d3359ecf40313efefa078d6fd0a87c70b
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-09-01 11:25:16 +0600

    add

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,12 @@
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
 import preprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-auto'
+import node_adapter from "@sveltejs/adapter-node";
+import static_adapter from "@sveltejs/adapter-static";
+import vercel from "@sveltejs/adapter-vercel";
+
+const is_static = process.env.BUILD_STATIC_ENV ?? false;
+const is_node = process.env.BUILD_NODE_ENV ?? false;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -17,7 +22,21 @@ const config = {
   ],
 
   kit: {
-    adapter: adapter(),
+    adapter: is_static
+      ? static_adapter({
+          fallback: "app.html",
+          // precompress: true,
+          strict: true
+      })
+    : is_node
+      ? node_adapter({
+          precompress: false
+      })
+    : vercel({
+          // an array of dependencies that esbuild should treat
+          // as external when bundling functions
+          external: []
+      }),
 
     // remove this if you don't want prerendering
     prerender: {
