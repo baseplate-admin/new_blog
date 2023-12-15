3296378403c177d2aab6f2693a7f43e71cc7f82c
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-15 09:57:08 +0600

    add

--- src/routes/post/[slug]/+page.server.ts
+++ src/routes/post/[slug]/+page.server.ts
@@ -6,14 +6,14 @@ export const load: PageServerLoad = ({ params,url }) => {
   const { slug } = params
 
   if(url.pathname.includes('how-i-created-alpine-blur-effect-using-svelte')){
-    throw redirect(302,'how-i-created-svelte-blur-effect-using-alpine')
+    redirect(302,'how-i-created-svelte-blur-effect-using-alpine');
   }
 
   // get post with metadata
   const post = posts.find((post) => slug === post.slug)
 
   if (!post) {
-    throw error(404, 'Post not found')
+    error(404, 'Post not found');
   }
 
   return {
